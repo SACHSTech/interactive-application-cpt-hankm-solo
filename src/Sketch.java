6bded55aa9955dd559da5b70dfae5fac50a12cfa
HankMa-1 <zihan.ma26@ycdsbk12.ca>
2026-06-03 12:31:56 -0400

    Create the method that draws the moving bar

--- src/Sketch.java
+++ src/Sketch.java
@@ -11,17 +11,39 @@ public class Sketch extends PApplet {
 
     @Override
     public void settings() {
-        size(600, 400); 
+        size(800, 800); 
     }
+
+    int barX = 325;
+    int barWidth = 150;
 
     @Override
     public void setup() {
-
+        
     }
 
     @Override
     public void draw() {
+        background (300, 200, 100);
+        
+        if(keyPressed) {
+            if(keyCode == LEFT) {
+                if(barX > 0) {
+                    barX -= 5;
+                }
+            } else if (keyCode == RIGHT){
+                if(barX < width - barWidth) {
+                    barX += 5;
+                }
+            }
+        }
 
+        drawBar(barX);
+    }
+
+    private void drawBar(int x){
+        fill(255);
+        rect(x, 700, 150, 20, 10);
     }
 
     /** Additional helper methods below */
